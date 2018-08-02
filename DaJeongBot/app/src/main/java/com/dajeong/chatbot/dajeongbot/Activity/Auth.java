a28fab626fb2bb041b3593a6c14b18bb793d58f1
suhyeon <suhyeon917917@gmail.com>
2018-08-02 18:33:32 +0900

    commit

--- DaJeongBot/app/src/main/java/com/dajeong/chatbot/dajeongbot/Activity/Auth.java
+++ DaJeongBot/app/src/main/java/com/dajeong/chatbot/dajeongbot/Activity/Auth.java
@@ -8,4 +8,5 @@ public class Auth {
     //구글 API 키
     public static final String KEY = "AIzaSyDXHHOgQhUeZb1XfUtuFS-lPXb23FIM4CI";
     public static final String[] SCOPES = {Scopes.PROFILE, YouTubeScopes.YOUTUBE};
+    fjdaskhfkdhfkqhfskqh
 }
