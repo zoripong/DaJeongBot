05b8fe2605cdcdc6be106f9ca74cd00b940890b2
Yuri Lee <yurilee9696@gmail.com>
2018-09-19 18:36:01 +0900

    schedule delete dialog add

--- DaJeongBot/app/src/main/java/com/dajeong/chatbot/dajeongbot/adapter/EventAdapter.java
+++ DaJeongBot/app/src/main/java/com/dajeong/chatbot/dajeongbot/adapter/EventAdapter.java
@@ -1,11 +1,16 @@
 package com.dajeong.chatbot.dajeongbot.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +20,10 @@ import com.daimajia.swipe.SimpleSwipeListener;
 import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
 import com.daimajia.swipe.implments.SwipeItemRecyclerMangerImpl;
+import com.dajeong.chatbot.dajeongbot.activity.CalendarActivity;
+import com.dajeong.chatbot.dajeongbot.activity.SettingActivity;
+import com.dajeong.chatbot.dajeongbot.customize.LogoutDialog;
+import com.dajeong.chatbot.dajeongbot.customize.ScheduleDeleteDialog;
 import com.dajeong.chatbot.dajeongbot.model.Event;
 import com.dajeong.chatbot.dajeongbot.R;
 
@@ -59,31 +68,6 @@ public class EventAdapter extends RecyclerSwipeAdapter<EventAdapter.SimpleViewHo
         }
     }
 
-
-//    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
-//        SwipeLayout swipeLayout;
-//        TextView textViewPos;
-//        TextView textViewData;
-//        Button buttonDelete;
-//
-//        public SimpleViewHolder(View itemView) {
-//            super(itemView);
-//            swipeLayout = (SwipeLayout) itemView.findViewById(R.id.swipe);
-//            textViewPos = (TextView) itemView.findViewById(R.id.position);
-//            textViewData = (TextView) itemView.findViewById(R.id.text_data);
-//            buttonDelete = (Button) itemView.findViewById(R.id.delete);
-//
-//            itemView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    Log.d(getClass().getSimpleName(), "onItemSelected: " + textViewData.getText().toString());
-//                    Toast.makeText(view.getContext(), "onItemSelected: " + textViewData.getText().toString(), Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//        }
-//    }
-
-
     @Override
     public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_row_item, parent, false);
@@ -114,7 +98,7 @@ public class EventAdapter extends RecyclerSwipeAdapter<EventAdapter.SimpleViewHo
         viewHolder.swipeLayout.addSwipeListener(new SimpleSwipeListener() {
             @Override
             public void onOpen(SwipeLayout layout) {
-                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.tvDelete));
+                //YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.tvDelete));
             }
         });
         viewHolder.swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
@@ -126,40 +110,51 @@ public class EventAdapter extends RecyclerSwipeAdapter<EventAdapter.SimpleViewHo
         viewHolder.tvDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mItemManger.removeShownLayouts(viewHolder.swipeLayout);
-                EventInfoArrayList.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, EventInfoArrayList.size());
-                mItemManger.closeAllItems();
-                Toast.makeText(view.getContext(), "Deleted ", Toast.LENGTH_SHORT).show();
+                //지우기 버튼을 클릭하면 다이얼로그를 띄움
+
+                // custom dialog
+                final Dialog dialog = new Dialog(mContext);
+                dialog.setContentView(R.layout.dialog_logout);
+                DisplayMetrics dm = mContext.getResources().getDisplayMetrics(); //디바이스 화면크기를 구하기위해
+                int width = (int)(dm.widthPixels*0.8f); //디바이스 화면 너비
+                //int height = dm.heightPixels; //디바이스 화면 높이
+
+                WindowManager.LayoutParams wm = dialog.getWindow().getAttributes();  //다이얼로그의 높이 너비 설정하기위해
+                wm.copyFrom(dialog.getWindow().getAttributes());  //여기서 설정한값을 그대로 다이얼로그에 넣겠다는의미
+                wm.width = width;
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+                TextView deleteBtn = (TextView) dialog.findViewById(R.id.tvOk);
+                TextView dialogText = (TextView) dialog.findViewById(R.id.dialogText);
+                TextView cancleBtn = (TextView) dialog.findViewById(R.id.tvCancle);
+                deleteBtn.setText("삭제");
+                dialogText.setText("일정을 삭제하시겠습니까?");
+
+                deleteBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+                        EventInfoArrayList.remove(position);
+                        notifyItemRemoved(position);
+                        notifyItemRangeChanged(position, EventInfoArrayList.size());
+                        mItemManger.closeAllItems();
+                    }
+                });
+
+                cancleBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
             }
         });
         //mItemManger.bind(viewHolder.itemView, position);
         mItemManger.bindView (viewHolder.itemView, position);
     }
-
-//        @Override
-//    public void onBindViewHolder( RecyclerView.ViewHolder holder, int position) {
-//        // Event event = mEvents.get(position);
-//        MyViewHolder myViewHolder = (MyViewHolder) holder;
-//        switch (holder.getItemViewType()){
-//            case 0: //내용
-//                myViewHolder.tvScheduleWhen.setText(EventInfoArrayList.get(position).scheduleWhen);
-//                myViewHolder.tvScheduleWhere.setText(EventInfoArrayList.get(position).scheduleWhere);
-//                myViewHolder.tvScheduleWhat.setText(EventInfoArrayList.get(position).scheduleWhat);
-//                myViewHolder.ivEventImage.setVisibility(View.GONE);
-//                break;
-//            case 1: //내용+사진
-//                myViewHolder.tvScheduleWhen.setText(EventInfoArrayList.get(position).scheduleWhen);
-//                myViewHolder.tvScheduleWhere.setText(EventInfoArrayList.get(position).scheduleWhere);
-//                myViewHolder.tvScheduleWhat.setText(EventInfoArrayList.get(position).scheduleWhat);
-//                myViewHolder.ivEventImage.setImageResource(EventInfoArrayList.get(position).drawableId);
-//                break;
-//            default:
-//                return;
-//        }
-//    }
-
 
 
     @Override
